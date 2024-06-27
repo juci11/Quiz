a9a599cdbdcc816c2fda03fc970b6a6366526719
juci11 <160618137+juci11@users.noreply.github.com>
2024-06-27 16:09:34 -0300

    Create questions.js

---
+++ questions.js
@@ -0,0 +1,90 @@
+let questions = [
+  {
+    numb: 1,
+    question: "Coloque o acento na palavra correta: Palavra: 'bebe'",
+    answer: "bebê",
+    options: [
+      "bebé",
+      "bebê",
+      "bêbe",
+      "bebè"
+    ]
+  },
+  {
+    numb: 2,
+    question: "Escreva a palavra corretamente acentuada: Palavra: 'pais'",
+    answer: "país",
+    options: [
+      "páis",
+      "país",
+      "pãis",
+      "pais"
+    ]
+  },
+  {
+    numb: 3,
+    question: "Identifique o erro de acentuação: Palavra: 'maquina'",
+    answer: "máquina",
+    options: [
+      "máquína",
+      "máquiná",
+      "máquina",
+      "maquína"
+    ]
+  },
+  {
+    numb: 4,
+    question: "Complete a palavra com o acento correto: Palavra: 'acoes'",
+    answer: "ações",
+    options: [
+      "acoés",
+      "açõés",
+      "ações",
+      "açoes"
+    ]
+  },
+  {
+    numb: 5,
+    question: "Reescreva a palavra com a acentuação adequada: Palavra: 'relogio'",
+    answer: "relógio",
+    options: [
+      "relógio",
+      "relógío",
+      "rélogio",
+      "relógiô"
+    ]
+  },
+  {
+    numb: 6,
+    question: "Marque o local onde deve ser colocado o acento: Palavra: 'exercito'",
+    answer: "exército",
+    options: [
+      "éxercito",
+      "exercíto",
+      "exércîto",
+      "exército"
+    ]
+  },
+  {
+    numb: 7,
+    question: "Escreva a palavra acentuada corretamente: Palavra: 'mapa'",
+    answer: "mapá",
+    options: [
+      "mápá",
+      "mapá",
+      "mápa",
+      "mapeá"
+    ]
+  },
+  {
+    numb: 8,
+    question: "Indique a posição correta do acento: Palavra: 'rapida'",
+    answer: "rápida",
+    options: [
+      "rápída",
+      "rápída",
+      "rápida",
+      "rápiâda"
+    ]
+  },
+];
